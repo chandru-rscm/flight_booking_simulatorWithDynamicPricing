eb8ed61ba84d61b1fbae45729c812accd5bb2389
Chandra Mohan RS <chandramohanrs218@gmail.com>
2026-01-10 22:22:36 +0530

    render url updation

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,8 +1,8 @@
 // OPTION 1: Localhost (Use this if your backend is running in a terminal on your PC)
-const API_BASE = "http://127.0.0.1:8000"; 
+//const API_BASE = "http://127.0.0.1:8000"; 
 
 // OPTION 2: Live Server (Use this if your backend is deployed online)
-// const API_BASE = "https://skybook-app.onrender.com"; 
+const API_BASE = "https://skybook-app.onrender.com"; 
 
 async function apiGet(path) {
     try {
